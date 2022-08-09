aeb54d3840e222134fc8191fa7f0cb08743055da
JEAR2 <jhedacro@gmail.com>
2022-08-08 22:47:25 -0500

    Se crear el modelo para preguntas

--- infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofka/mongo/course/MongoRepositoryAdapterCourse.java
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofka/mongo/course/MongoRepositoryAdapterCourse.java
@@ -7,11 +7,11 @@ import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MongoRepositoryAdapter extends AdapterOperations<Course, CourseDocument, String, MongoDBRepository>
+public class MongoRepositoryAdapterCourse extends AdapterOperations<Course, CourseDocument, String, MongoDBRepositoryCourse>
  implements CourseRepository
 {
 
-    public MongoRepositoryAdapter(MongoDBRepository repository, ObjectMapper mapper) {
+    public MongoRepositoryAdapterCourse(MongoDBRepositoryCourse repository, ObjectMapper mapper) {
         /**
          *  Could be use mapper.mapBuilder if your domain model implement builder pattern
          *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
