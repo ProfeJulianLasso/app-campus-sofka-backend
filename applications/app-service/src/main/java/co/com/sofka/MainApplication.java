b26f41326f7348411416812e6dac990f98ac473a
danielgil <danistcruz@gmail.com>
2022-08-10 22:10:03 -0500

    se modifica caso de uso de crear Step

--- applications/app-service/src/main/java/co/com/sofka/MainApplication.java
+++ applications/app-service/src/main/java/co/com/sofka/MainApplication.java
@@ -1,11 +1,13 @@
 package co.com.sofka;
 
-import co.com.sofka.model.step.gateways.StepRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"co.com.sofka.model.step.gateways"})
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
--- domain/usecase/src/main/java/co/com/sofka/usecase/createstep/CreateStepUseCase.java
+++ domain/usecase/src/main/java/co/com/sofka/usecase/createstep/CreateStepUseCase.java
@@ -16,30 +16,26 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
+
 @RequiredArgsConstructor
-public class CreateStepUseCase {
+public class CreateStepUseCase implements Function<InitialCourse, Mono<InitialCourse>> {
 
    private final StepRepository stepRepository;
 
-   public Mono<Step> createStep (Step step){
-      return stepRepository.save(step);
-   }
-
-
-//    @Override
-//    public Mono<InitialCourse> apply(InitialCourse initialCourse) {
-//        JSONArray jsonarray = new JSONArray(initialCourse.getSteps());
-//        AtomicInteger index = new AtomicInteger();
-//        Flux.fromIterable(jsonarray)
-//                .flatMap(element -> {
-//                    Integer x = index.getAndIncrement();
-//                    JSONObject objeto = jsonarray.getJSONObject(x);
-//                    Set<String> stepss = jsonarray.getJSONObject(x).keySet();
-//                    String step = stepss.toString().replace("[","").replace("]","");
-//                    System.out.println(step);
-//                    Step stepSave = new Step();
-//                    return stepRepository.save(stepSave);
-//                }).subscribe();
-//        return Mono.just(initialCourse);
-//    }
+    @Override
+    public Mono<InitialCourse> apply(InitialCourse initialCourse) {
+        JSONArray jsonarray = new JSONArray(initialCourse.getSteps());
+        AtomicInteger index = new AtomicInteger();
+        Flux.fromIterable(jsonarray)
+                .flatMap(element -> {
+                    Integer x = index.getAndIncrement();
+                    JSONObject objeto = jsonarray.getJSONObject(x);
+                    Set<String> stepss = jsonarray.getJSONObject(x).keySet();
+                    String step = stepss.toString().replace("[","").replace("]","");
+                    System.out.println(step);
+                    Step stepSave = new Step();
+                    return stepRepository.save(stepSave);
+                }).subscribe();
+        return Mono.just(initialCourse);
+    }
 }
