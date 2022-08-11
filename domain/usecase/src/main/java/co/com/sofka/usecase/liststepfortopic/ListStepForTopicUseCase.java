3de717dcc14180442c3a29b4e70361f3f760035c
danielgil <danistcruz@gmail.com>
2022-08-11 16:20:06 -0500

    se organiza el caso de uso de listar StepForTopic

--- domain/usecase/src/main/java/co/com/sofka/usecase/liststepfortopic/ListStepForTopicUseCase.java
+++ domain/usecase/src/main/java/co/com/sofka/usecase/liststepfortopic/ListStepForTopicUseCase.java
@@ -5,11 +5,8 @@ import co.com.sofka.model.stepfortopic.gateways.StepForTopicRepository;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.function.BiFunction;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class ListStepForTopicUseCase implements BiFunction<String, String, Flux<StepForTopic>> {
@@ -17,20 +14,9 @@ public class ListStepForTopicUseCase implements BiFunction<String, String, Flux<
 
     @Override
     public Flux<StepForTopic> apply(String idStep, String idTopic) {
-        List<StepForTopic> list = new ArrayList<>();
         Flux<StepForTopic> stepp = stepForTopicRepository.findAll();
-        System.out.println(stepp);
-            stepp
-                    .map(element -> {
-                        list.add(element);
-                        return element;
-                    });
-              //  .filter(stepForTopic -> Objects.equals(stepForTopic.getIdTopic(), idTopic))
-                  //  .map(element -> list.add(new StepForTopic()))
-                   // .filter(stepForTopic -> Objects.equals(stepForTopic.getIdStep(), idStep)).flatMap(element->)
-
-        System.out.println(list);
-        return stepp;
-
+        return stepp
+                .filter(stepForTopic -> Objects.equals(stepForTopic.getIdTopic(), idTopic))
+                    .filter(stepForTopic -> Objects.equals(stepForTopic.getIdStep(), idStep));
     }
 }
