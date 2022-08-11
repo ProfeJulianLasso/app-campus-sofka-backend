b424ba68f04f46d7872ec6874bc194254fca0018
JEAR2 <jhedacro@gmail.com>
2022-08-10 22:11:17 -0500

    Se elimina el modelo step para modificacion

--- domain/model/src/main/java/co/com/sofka/model/step/gateways/StepRepository.java
+++ domain/model/src/main/java/co/com/sofka/model/step/gateways/StepRepository.java
@@ -3,8 +3,6 @@ package co.com.sofka.model.step.gateways;
 import co.com.sofka.model.step.Step;
 import reactor.core.publisher.Mono;
 
-
 public interface StepRepository {
-
     Mono<Step> save(Step step);
 }
