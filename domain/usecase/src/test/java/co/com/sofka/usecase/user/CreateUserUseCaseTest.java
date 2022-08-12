f9c366b9cf111da512fee00c0134d09284928011
yeferson1221 <alejandro.yandd@gmail.com>
2022-08-12 11:38:50 -0500

    test user terminado

--- domain/usecase/src/test/java/co/com/sofka/usecase/user/CreateUserUseCaseTest.java
+++ domain/usecase/src/test/java/co/com/sofka/usecase/user/CreateUserUseCaseTest.java
@@ -3,18 +3,20 @@ package co.com.sofka.usecase.user;
 import co.com.sofka.model.course.Course;
 import co.com.sofka.model.user.User;
 import co.com.sofka.model.user.gateways.UserRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class CreateUserUseCaseTest {
     @InjectMocks
     private CreateUserUseCase createUserUseCaseTest;
@@ -22,21 +24,17 @@ class CreateUserUseCaseTest {
     @Mock
     private UserRepository userRepositoryTest;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
     @Test
     void createUserTest(){
-        Course Set;
         User user = User.builder()
                 .id("erf")
                 .email("yefer@gmail.com")
                 .build();
 
-        when(userRepositoryTest.save(user)).thenReturn(Mono.just(user));
+        when(userRepositoryTest.save(Mockito.any(User.class))).thenReturn(Mono.just(user));
 
         StepVerifier.create(createUserUseCaseTest.createUser(user))
+                .expectNext(user)
                 .expectComplete()
                 .verify();
 
